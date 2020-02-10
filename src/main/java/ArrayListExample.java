127ed12c7f353fee350e9ee64840e7c73fbbc7bf
Eldar Kalachev <rusgunter@gmail.com>
2020-02-10 21:07:52 +0300

    ArrayList

---
+++ src/main/java/ArrayListExample.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> games = new ArrayList<>();
+        games.add("game 1"); // time complexity O(1)
+        games.add("game 2"); // time complexity O(1)
+        games.add(0, "game 3"); // time complexity O(n)
+    }
+}
